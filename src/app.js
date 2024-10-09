50209b2cdb4a76a10f80b84315184f25262479d8
Nani-Prabhakar <nanibramhasamudram@gmail.com>
2024-10-09 21:51:04 +0530

    CRUD APIs on DB ,schema validations

--- src/app.js
+++ src/app.js
@@ -2,25 +2,61 @@ const express=require('express')
 const connectDB=require('./config/database')
 const User=require('./models/userModel')
 const app=express()
-connectDB().then(()=>{
-    console.log("database connected successfully!!")
-}).catch(err=>{
-    console.log("database cannot be connected")
-})
+
 app.use(express.json())
 
-
+app.get("/user",async(req,res)=>{
+    const emailID=req.body.email;
+    try{
+      const user =await User.findOne({email:emailID})
+      if(!user){
+        res.status(404).send("user not found")
+      }else {
+        res.status(200).send(user)
+      }
+    }catch(err){
+        res.status(404).send("user not found")
+    }
+   
+    
+})
 app.post("/signup",async (req,res)=>{
     const data=req.body;
     const user =new User(data);
     try{
         await user.save();
     }catch(err){
-        console.log("error in creating user:",err.message)
+      
+        res.send("error in creating user:"+err.message)
     }
     
     res.send("user created successfully")
 })
-app.listen(7777,()=>{
-    console.log("server listening at port 7777")
+app.patch("/update",async (req,res)=>{
+  const id=req.body.id;
+  const data=req.body;
+  const user=new User;
+  try{
+    const user=await User.findByIdAndUpdate({_id:id},  data, {
+      returnDocument:"after",
+      runValidators:true,
+    });
+    //await doc.save();
+    console.log(user)
+    res.send("data updated successfully")
+  }catch(err){
+    console.log("error in creating user:",err.message)
+  }
+  
+
 })
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,41 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({
-    firstName:{
-        type:String
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        minLength: 4,
+        maxLength: 50,
+        required: true,
     },
-    lastName:{
-        type:String
+    lastName: {
+        type: String,
+        minLength: 4,
+        maxLength: 50,
+        required: true,
     },
-    email:{
-        type:String
+    email: {
+        type: String,
+        unique: true,
+        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        required: true,
     },
-    password:{
-        type:String
+    password: {
+        type: String,
+        match: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+        required: true,
     },
-    age:{
-        type:Number
+    age: {
+        type: Number,
+        min: 12,
+        required: true,
     },
-    gender:{
-        type:String
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+        required: true,
     }
-})
+}, {
+    timestamps: true
+});
 
-module.exports=mongoose.model("User",userSchema)
+module.exports = mongoose.model("User", userSchema);
